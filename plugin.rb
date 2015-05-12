2c5d922e947d813be6bc6238c1fbd5eef984f942
Neil Lalonde <neillalonde@gmail.com>
2015-05-12 10:18:38 -0400

    Add metadata

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,9 @@
+# name: twitter-profile-link
+# about: Link to twitter profile on user pages
+# version: 0.1
+# authors: Neil Lalonde
+# url: https://github.com/discourse/twitter-profile-link
+
 register_asset "javascripts/discourse/templates/connectors/user-profile-primary/twitter-profile.js.handlebars"
 register_asset "stylesheets/twitter-profile-link.scss"
 
